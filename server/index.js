b455106023cc2f4a8c40468d06e1bf01ddd3cbd6
lohit <lohitkolluri@gmail.com>
2023-09-22 16:20:34 +0530

    API Changes 👽️

--- server/index.js
+++ server/index.js
@@ -1,45 +1,48 @@
 const express = require("express");
 const cors = require("cors");
-const Axios = require("axios");
+const axios = require("axios");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/compile", (req, res) => {
-	//getting the required data from the request
-	let code = req.body.code;
-	let language = req.body.language;
-	let input = req.body.input;
+app.post("/compile", async (req, res) => {
+  try {
+    const { code, language, input } = req.body;
 
-	if (language === "python") {
-		language = "py"
-	}
+    // Map language to its corresponding code
+    const languageMap = {
+      python: "py",
+      // Add more mappings as needed
+    };
 
-	let data = ({
-		"code": code,
-		"language": language,
-		"input": input
-	});
-	let config = {
-		method: 'post',
-		url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		data: data
-	};
-	//calling the code compilation API
-	Axios(config)
-		.then((response) => {
-			res.send(response.data)
-			console.log(response.data)
-		}).catch((error) => {
-			console.log(error);
-		});
-})
+    const mappedLanguage = languageMap[language] || language;
 
-app.listen(process.env.PORT || PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+    const data = new URLSearchParams();
+    data.append("code", code);
+    data.append("language", mappedLanguage);
+    data.append("input", input);
+
+    const config = {
+      method: "post",
+      url: "https://api.codex.jaagrav.in",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: data,
+    };
+
+    const response = await axios(config);
+
+    res.send(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
