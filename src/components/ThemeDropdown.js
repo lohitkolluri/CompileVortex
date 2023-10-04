6ceec8a6ec3b403cbafd7981192971c405f34ac1
Lohit Kolluri <lohitkolluri@Lohits-MacBook-Pro.local>
2023-10-04 09:00:29 +0530

    Visual Change

--- src/components/ThemeDropdown.js
+++ src/components/ThemeDropdown.js
@@ -4,14 +4,16 @@ import monacoThemes from "monaco-themes/themes/themelist";
 import { customStyles } from "../constants/customStyles";
 
 const ThemeDropdown = ({ handleThemeChange, theme }) => {
+  const themeOptions = Object.entries(monacoThemes).map(([themeId, themeName]) => ({
+    label: themeName,
+    value: themeId,
+    key: themeId,
+  }));
+
   return (
     <Select
-      placeholder={`Select Theme`}
-      options={Object.entries(monacoThemes).map(([themeId, themeName]) => ({
-        label: themeName,
-        value: themeId,
-        key: themeId,
-      }))}
+      placeholder="Select Theme"
+      options={themeOptions}
       value={theme}
       styles={customStyles}
       onChange={handleThemeChange}
